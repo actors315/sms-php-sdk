72e3613ca89150c44c3a5165c552c67a7e275188
actors315 <actors315@gmail.com>
2016-11-05 14:52:52 +0800

    '创建版本'

---
+++ src/Heysky/Client.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiehuanjin
+ * Date: 2016/11/5
+ * Time: 14:27
+ */
+
+namespace Heysky;
+
+
+class Client
+{
+    /**
+     * API Endpoints
+     *
+     * @var array
+     */
+    private static $api = array(
+        "heysky" => "https://api.leancloud.cn",
+    );
+}
---
+++ src/Heysky/SMS.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiehuanjin
+ * Date: 2016/11/5
+ * Time: 14:28
+ */
+
+namespace Heysky;
+
+/**
+ * Class SMS
+ *
+ * 封装Heysky国际短信API
+ * @package Heysky
+ */
+class SMS
+{
+
+}
