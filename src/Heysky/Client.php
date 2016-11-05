a6a2369a325886f2550ebd1241dfa96bc2a797f8
actors315 <actors315@gmail.com>
2016-11-05 16:13:40 +0800

    relase

--- src/Heysky/Client.php
+++ src/Heysky/Client.php
@@ -17,6 +17,79 @@ class Client
      * @var array
      */
     private static $api = array(
-        "heysky" => "https://api.leancloud.cn",
+        "heysky" => "http://api2.santo.cc/submit",
     );
+
+    /**
+     * 第三方发送平台
+     *
+     * @var string
+     */
+    private static $plat = 'heysky';
+
+    /**
+     * 短信平台用户名
+     *
+     * @var string
+     */
+    private static $appId;
+
+    /**
+     * 短信平台用户密码
+     *
+     * @var string
+     */
+    private static $appSecret;
+
+    /**
+     * 自定义发送者号码
+     *
+     * @var string
+     */
+    private static $sa;
+
+    public static function initialize($appId, $appSecret, $sa)
+    {
+        self::$appId = $appId;
+        self::$appSecret = $appSecret;
+        self::$sa = $sa;
+    }
+
+    public static function getAPIEndPoint()
+    {
+        return self::$api[self::$plat];
+    }
+
+    public static function request($method, $path, $data)
+    {
+        $url = self::getAPIEndPoint();
+        $url .= $path;
+
+        $data['cpid'] = self::$appId;
+        $data['cppwd'] = self::$appSecret;
+        $data['sa'] = self::$sa;
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        switch ($method) {
+            case "GET":
+                    $url .= '?'.http_build_query($data);
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                break;
+        }
+        $response = curl_exec($ch);
+        curl_close($ch);
+        parse_str($response, $response_array);
+        return $response_array;
+    }
+
+    public static function post($path, $data)
+    {
+        return self::request('POST', $path, $data);
+    }
+
+    public static function get($path, $data)
+    {
+        return self::request('GET', $path, $data);
+    }
 }
--- src/Heysky/SMS.php
+++ src/Heysky/SMS.php
@@ -16,5 +16,101 @@ namespace Heysky;
  */
 class SMS
 {
+    /**
+     * 操作命令
+     * MT_REQUEST 单目标号码信息下行
+     * MULTI_MT_REQUEST 多目标号码信息下行
+     *
+     * @var string
+     */
+    private $command = 'MT_REQUEST';
 
+    /**
+     * 目标号码，MSISDN
+     *
+     * @var string
+     */
+    private $da;
+
+    /**
+     * 消息内容编码
+     * 15: GBK
+     * 8: Unicode
+     * 0: ISO8859-1
+     *
+     * @var string
+     */
+    private $dc = 15;
+
+    /**
+     * @var 消息内容
+     */
+    private $sm;
+
+    function setCommand($command){
+        $this->command = $command;
+    }
+
+    function setDa($da){
+        $this->da[] = $da;
+    }
+
+    function setDc($dc){
+        if (in_array($dc, [15, 8, 0])) {
+            $this->dc = $dc;
+        }
+    }
+
+    function setSm($sm){
+        $this->sm = $sm;
+    }
+
+    function send(){
+        $data = $this->encode();
+        $resp = Client::post("", $data);
+        return $resp;
+    }
+
+    private function encode(){
+        $this->da = array_unique($this->da);
+        count($this->da) > 1 && $this->setCommand('MULTI_MT_REQUEST');
+
+        return [
+            'command'   => $this->command,
+            'da' => implode(',',$this->da),
+            'dc' => $this->dc,
+            'sm' => $this->encodeHexStr('',$this->sm)
+        ];
+    }
+
+    /**
+     *  decode Hex String
+     *
+     * @param string $dataCoding       charset
+     * @param string $hexStr      convert a hex string to binary string
+     * @return string binary string
+     */
+    private function decodeHexStr($dataCoding, $hexStr)
+    {
+        $hexLenght = strlen($hexStr);
+
+        $binString = '';
+        for ($x = 1; $x <= $hexLenght/2; $x++)
+        {
+            $binString .= chr(hexdec(substr($hexStr,2 * $x - 2,2)));
+        }
+
+        return $binString;
+    }
+
+    /**
+     * encode Hex String
+     *
+     * @param string $dataCoding
+     * @param string $realStr
+     * @return string hex string
+     */
+    private function encodeHexStr($dataCoding, $realStr) {
+        return bin2hex($realStr);
+    }
 }
